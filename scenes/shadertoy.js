78d208b7dd24a7a964bd2dd2e12892e54f29f3c6
Marcio T <mlt4356-github@yahoo.com>
2017-04-02 19:46:14 -0600

    - Added support for cubemaps in shadertoy loader. - Added many more shadertoy demos.

--- scenes/shadertoy.js
+++ scenes/shadertoy.js
@@ -1,7 +1,5 @@
-/* The shader code is a slightly modified version of Shane's "Abstract Corridor"
- * shader, CC-NC-SA-3.0:
- *
- *   https://www.shadertoy.com/view/MlXSWX
+/* This is an experimental loader for ShaderToys. It will attempt to load
+ * the shader indicated by the "shader" variable in the URL.
  */
 RendererConfig.dome.fullSphere    = false;
 
@@ -23,28 +21,75 @@ function setupScene(scene) {
             console.log("Warning: Multiple render passes not currently supported");
         }
 
+        var material;
         var fragmentShader = shader.renderpass[0].code;
         var shaderInputs   = shader.renderpass[0].inputs;
         var textures       = [];
+        var samplerTypes   = ["sampler2D", "sampler2D", "sampler2D", "sampler2D"];
+
         for(var i = 0; i < shaderInputs.length; i++) {
-            if(shaderInputs[i].ctype == "texture") {
-                var texture = textureLoader.load("../textures/shadertoy" + shaderInputs[i].src);
-                if(shaderInputs[i].sampler.wrap == "repeat") {
-                    texture.wrapS = THREE.RepeatWrapping;
-                    texture.wrapT = THREE.RepeatWrapping;
-                }
-                textures[shaderInputs[i].channel] = texture;
-                console.log("Sampler:", shaderInputs[i].sampler);
+            console.log("ShaderInput:", i, shaderInputs[i].ctype);
+            switch (shaderInputs[i].ctype) {
+                case "texture":
+                    function textureLoaded(i, texture) {
+                        material.uniforms.iChannelResolution.value[i].x = texture.image.width;
+                        material.uniforms.iChannelResolution.value[i].y = texture.image.height;
+                    }
+                    var texture = textureLoader.load(
+                        "../textures/shadertoy" + shaderInputs[i].src,
+                        textureLoaded.bind(null, i)
+                    );
+                    if(shaderInputs[i].sampler.wrap == "repeat") {
+                        texture.wrapS = THREE.RepeatWrapping;
+                        texture.wrapT = THREE.RepeatWrapping;
+                    }
+                    textures[shaderInputs[i].channel] = texture;
+                    console.log("Sampler:", shaderInputs[i].sampler);
+                    break;
+                case "cubemap":
+                    var filename = shaderInputs[i].src.replace(/\.([a-z]+$)/,"\_NUM.$1");
+                    var texture = new THREE.CubeTextureLoader()
+                        .setPath('../textures/shadertoy')
+                        .load(
+                            [
+                                filename.replace("_NUM",""),
+                                filename.replace("_NUM","_1"),
+                                filename.replace("_NUM","_2"),
+                                filename.replace("_NUM","_3"),
+                                filename.replace("_NUM","_4"),
+                                filename.replace("_NUM","_5")
+                            ]
+                        );
+                    textures[shaderInputs[i].channel] = texture;
+                    samplerTypes[shaderInputs[i].channel] = "samplerCube";
+                    break;
             }
         }
 
-        var material = new THREE.RawShaderMaterial( {
+        for(var i = 0; i < 4; i++) {
+            var slotLabel = "_slot" + i + "_";
+            fragmentShaderPreamble = fragmentShaderPreamble.replace(slotLabel, samplerTypes[i]);
+            if(samplerTypes[i] == "samplerCube") {
+                var regex =new RegExp("texture\\(iChannel"+i,"g");
+                fragmentShader = fragmentShader.replace(regex, "textureCube(iChannel"+i);
+            }
+        }
+
+        material = new THREE.RawShaderMaterial( {
             vertexShader:   vertexShader,
             fragmentShader: fragmentShaderPreamble + fragmentShader,
             uniforms: {
                 iGlobalTime: {value: 1.0 },
                 iChannel0:   {value: textures[0] },
                 iChannel1:   {value: textures[1] },
+                iChannel2:   {value: textures[2] },
+                iChannel3:   {value: textures[3] },
+                iChannelResolution:    {value: [
+                    new THREE.Vector3(256, 256),
+                    new THREE.Vector3(256, 256),
+                    new THREE.Vector3(256, 256),
+                    new THREE.Vector3(256, 256)
+                ]},
                 iResolution: {value: new THREE.Vector2(1024, 1024)},
                 iMouse:      {value: new THREE.Vector4(0, 0, 0, 0)},
             }
@@ -67,6 +112,9 @@ function setupScene(scene) {
     var query = parseQuery();
     if(query.shader !== null) {
         loadUrl("https://www.shadertoy.com/api/v1/shaders/" + query.shader + "?key=" + apiKey, processShader);
+    }
+    if(query.ambulatory) {
+        fragmentShaderPreamble = "#define AMBULATORY\n" + fragmentShaderPreamble;
     }
 
     var geometry = new THREE.SphereBufferGeometry(RendererConfig.dome.radius, 60, 40);
@@ -96,8 +144,11 @@ varying   vec2  vUV;
 uniform float     iGlobalTime;
 uniform vec2      iResolution;
 uniform vec4      iMouse;
-uniform sampler2D iChannel0;
-uniform sampler2D iChannel1;
+uniform _slot0_   iChannel0;
+uniform _slot1_   iChannel1;
+uniform _slot2_   iChannel2;
+uniform _slot3_   iChannel3;
+uniform vec3      iChannelResolution[4];
 
 void mainVR( out vec4 fragColor, in vec2 fragCoord, in vec3 ro, in vec3 rd );
 
@@ -110,7 +161,11 @@ void main()  {
     );
     //normalize(rd);
 
+#ifdef AMBULATORY
+    vec3 ro = vec3(0., 0., -iGlobalTime);
+#else
     vec3 ro = vec3(0., 0., 0.);
+#endif
     mainVR( gl_FragColor, vUV * vec2(1024), ro, rd );
 }
 */}.getComment();
