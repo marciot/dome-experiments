7ad1ffd2bdba4bb57be053a5dce40af9f98babe6
Marcio T <mlt4356-github@yahoo.com>
2017-09-19 18:23:56 -0600

    WebRTC fixes for iOS 11.

--- libs/peer.js/peer-modified.js
+++ libs/peer.js/peer-modified.js
@@ -1414,7 +1414,7 @@ Socket.prototype.close = function() {
 module.exports = Socket;
 
 },{"./util":8,"eventemitter3":9}],8:[function(require,module,exports){
-var defaultConfig = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
+var defaultConfig = {'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }]};
 var dataCount = 1;
 
 var BinaryPack = require('js-binarypack');
@@ -1543,7 +1543,7 @@ var util = peerjsUtil = {
       var reliablePC = new RTCPeerConnection(defaultConfig, {});
       try {
         var reliableDC = reliablePC.createDataChannel('_PEERJSRELIABLETEST', {});
-        sctp = reliableDC.reliable;
+        sctp = reliableDC.ordered;
       } catch (e) {
       }
       reliablePC.close();
